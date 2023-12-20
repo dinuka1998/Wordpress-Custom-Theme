2975c5c133c47c310abc637c15064aef4329deaa
Dinuka Amarasinghe <agpdinuka1998@gmail.com>
2023-12-20 21:18:08 +0530

    setup theme main file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+* Main template file
+* @package Codecrafts
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Codecrafts</title>
+</head>
+<body>
+    
+</body>
+</html>
